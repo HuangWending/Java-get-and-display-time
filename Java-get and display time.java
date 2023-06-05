14866abac2cf4dc4462fbe97c7e214046ee706f0
黃(黄)文定-ㄏㄨㄤˊ ㄨㄣˊㄉㄧㄥˋ <91007091+HuangWending@users.noreply.github.com>
2023-06-05 20:34:16 +0800

    Add files via upload

---
+++ Java-get and display time.java
@@ -0,0 +1,9 @@
+import java.util.Date;                  // 导入java.util包中的Date类
+
+public class 您的文件名 {        // 定义名为和文件名一样的公共类
+
+    public static void main(String[] args) {    // 主方法
+        Date currentTime = new Date();          // 创建一个Date对象，表示当前时间
+        System.out.println("Current time: " + currentTime);    // 打印当前时间
+    }
+}
